761de4e106b6573552c2e5e79b94b1b46fde0a2f
NadaSOsman <76102938+NadaSOsman@users.noreply.github.com>
2022-11-03 13:44:52 +0100

    Create test.sh

---
+++ scipts/test.sh
@@ -0,0 +1,8 @@
+# JAAD_all
+python3 run.py --config_file configs_all.yaml --test
+
+# JAAD_beh
+python3 run.py --config_file configs_beh.yaml --test
+
+# PIE
+python3 run.py --config_file configs_pie.yaml --test
